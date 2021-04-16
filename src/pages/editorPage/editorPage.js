b31c31b5f5e7cceb2a3ae11df1f7dfb84c3cc6c9
wojciech-bigosinski-abb <wojciech.bigosinski@pl.abb.com>
2021-04-17 01:44:04 +0200

    added editor

--- src/pages/editorPage/editorPage.js
+++ src/pages/editorPage/editorPage.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
 import './editorPage.css';
-import { Button, Badge, Row, Col, Form } from 'react-bootstrap';
+import { Button, Badge, Row, Col, Form, Container } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 
 export default class EditorPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       theme: "",
-      redirect: false
+      redirect: false,
+      editorState: EditorState.createEmpty(),
     };
 
     this.onThemeInput = this.onThemeInput.bind(this);
   }
 
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
 
   
   render() {
+    const { editorState } = this.state;
     return (
-          <header className="App-header">
-            <div className="beginRectangle">
-
-            </div>
-          </header>
+      <Container className="App-header">
+        <h1>Mental health</h1>
+        <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+      </Container>
     );
   }
 
